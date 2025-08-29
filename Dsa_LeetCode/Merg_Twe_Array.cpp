0f535b95777ceb919317d0ed4ab8055d6f097d4c
AbdulRafay-00 <iamrafay.00@gmail.com>
2025-08-29 05:11:54 -0500

    merg of two list changes

--- Dsa_LeetCode/Merg_Twe_Array.cpp
+++ Dsa_LeetCode/Merg_Twe_Array.cpp
@@ -1,96 +1,6 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-//  void merg_Function(vector<int>& ve , vector <int>& vec2){
-//     int n = ve.size();
-//     ve.erase(ve.end()-3 , ve.end());
-//     cout << "befor change"<< endl;
-//     for(int i : ve){
-//         cout<< i;
-
-//     }
-//     cout <<endl;
-//      for(int i = 0; i < vec2.size(); i++){
-//          ve.push_back(vec2[i]);
-//          n--;
-         
-//         }
-//         cout << "after change"<< endl;
-//         for(int i : ve){
-//             cout<< i;
-
-//         }
-
-//  }
-
-
-//  void merg_func2(vector<int> vec , vector <int> vec2){
-//     int vec_non_zero = 0;
-//     for(int i = 0; i< vec.size(); i++){
-//     if(vec[i] == 0){
-//                 break;
-//     }
-//     vec_non_zero ++;  
-//     }
-//     int j = vec2.size()-1;
-//     int i = vec.size()-1;
-//     cout<<
-
-//     // while (j >= 0 )
-//     // {
-//     //     cout << "in if loop"<< endl;
-// //     if(vec[vec_non_zero] > vec2[j]){
-// //         cout << "in lllllllllllllllllllllllllllllllllllllllll loop"<< endl;
-//             //         vec[i] = vec2[j];
-//     //         vec_non_zero--;
-//     //         i--;
-//     //         j--;
-
-//     //     }
-//     // }
-    
-//     for(int i :vec){
-//         cout<< i;
-//     }
-
-//  }
-
-// int main(){
-//     vector <int> vec = {1,2,3,0,0,0};
-//     vector <int> vec2 = {2,4,5};
-//     merg_func2(vec , vec2);
-// }
-
-
-
-
-
 #include <iostream>
 #include <vector>
 using namespace std;
- void merg_Function(vector<int>& ve , vector <int>& vec2){
-    int n = ve.size();
-    ve.erase(ve.end()-3 , ve.end());
-    cout << "befor change"<< endl;
-    for(int i : ve){
-        cout<< i;
-
-    }
-    cout <<endl;
-     for(int i = 0; i < vec2.size(); i++){
-         ve.push_back(vec2[i]);
-         n--;
-         
-        }
-        cout << "after change"<< endl;
-        for(int i : ve){
-            cout<< i;
-
-        }
-
- }
-
-
  void merg_func2(vector<int> vec , vector <int> vec2){
     int vec_non_zero = 0;
     for(int i = 0; i< vec.size(); i++){
@@ -110,20 +20,15 @@ using namespace std;
         cout << "in if loop"<< endl;
         if(vec[vec_non_zero] > vec2[j]){
             cout << "in lllllllllllllllllllllllllllllllllllllllll loop"<< endl;
-            int temp = vec[vec_non_zero];
-            vec[vec_non_zero] =vec2[j];
-            vec[i] = temp;
-            vec_non_zero--;
-            i--;
-            j--;            
+            // int temp = vec[vec_non_zero];
+            // vec[vec_non_zero] =vec2[j];
+            vec[i] =  vec[vec_non_zero];
+            vec_non_zero--;           
         }else{
             vec[i] = vec2[j];
-            i--;
             j--;
-            
-
-
         }
+        i--;
     }
 
     
@@ -133,6 +38,6 @@ using namespace std;
 
     int main(){
     vector <int> vec = {1,2,3,0,0,0};
-    vector <int> vec2 = {2,4,5};
+    vector <int> vec2 = {1,4,5};
     merg_func2(vec , vec2);
 }
