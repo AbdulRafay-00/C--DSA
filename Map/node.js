671d403adb0f76f4086fa5e80dce6f1e5f7df2a2
AbdulRafay-00 <iamrafay.00@gmail.com>
2025-09-09 03:52:51 -0500

    js

--- Map/node.js
+++ Map/node.js
@@ -1,1 +1,13 @@
 console.log("hello world ");
+const http = require("http");
+let a = 3;
+let b = 4;
+console.log(a  + b);
+const server = http.createServer((req, res) => {
+    console.log(req);
+
+});
+const port = 3000;
+server.listen( port, () => {
+    console.log('server running at http://localhost:${port} ');
+});
